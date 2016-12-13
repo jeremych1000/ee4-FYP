029d821dd011bc67f1c2745613ffd1546780c005
Jeremy Chan <jeremych1000@gmail.com>
2016-12-13 01:05:08 +0000

    woops empty file

---
+++ hardware/rpi/provision.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get -y install sudo cmake curl dos2unix emacs g++ gedit git git-core java-common java-package make perl python3 ssh 
+sudo apt-get -y libtiff5-dev libopencv-dev libjpeg-dev libssl-dev
+sudo apt-get -y mariadb-server mariadb-client apache2 php5 libapache2-mod-php5 php5-mysqlnd php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-apcu phpmyadmin
