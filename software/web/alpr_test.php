2d289a1c7286988cacf035f97e8893a33f41b9c1
Jeremy Chan <jeremych1000@gmail.com>
2016-12-15 23:50:47 +0000

    multiple frames working, need to put into array

--- software/web/alpr_test.php
+++ software/web/alpr_test.php
@@ -1,23 +1,64 @@
 <?php
 //header('Content-Type: application/json');
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+require_once 'db_functions.php';
 
 // outputs the username that owns the running php/httpd process
 // (on a system with the "whoami" executable in the path)
 $image_name = $_GET["n"];
 $region = $_GET["r"];
 
-require_once 'db_functions.php';
+if (empty($image_name)) {
+	die('Name cannot be empty, otherwise it will run on the entire directory!');
+}
 
-//echo $image_name;
-$exec_command = 'alpr -c '.$region.' --clock --detect_region'.' '.'upload/'.$image_name.' 2>&1';
-//passthru($exec_output, $exec_return, $exec_output);
+$exec_command = 'alpr --country '.$region.' --detect_region --json --topn 3'.' '.'upload/'.$image_name.' 2>&1';
 $exec_output = shell_exec($exec_command);
-//echo gettype($exec_output);
 
+//split video multiple json into array of json objects
+$json = json_split_objects($exec_output);
+my_print($json);
+
+echo "\n-----------------------------\n";
+
+//picture
+if (sizeof($json) == 1){
+	$exec_array = json_decode($json[0], true);
+	if (json_last_error() == 0){
+		my_print($exec_array);
+	}
+	else{
+		die('JSON error code: '.json_last_error().', refer <a href="http://php.net/manual/en/function.json-last-error.php">here</a>.');
+	}
+}
+//video
+else{
+	foreach ($json as $j) {
+	    $exec_array = json_decode($j, true);
+	    if (json_last_error() == 0){
+			my_print($exec_array);
+		}
+		else{
+			die('JSON error code: '.json_last_error().', refer <a href="http://php.net/manual/en/function.json-last-error.php">here</a>.');
+		}
+	}
+}
+
+echo "\n-----------------------------\n";
+
+
+
+//$exec_array = json_encode($exec_array, JSON_PRETTY_PRINT);
+//echo gettype($exec_array);
+
+//echo gettype($exec_array);
+//var_dump($exec_array);
+//print_r($exec_array, true);
 //$exec_json = json_encode($exec_output, JSON_PRETTY_PRINT);
-//echo pprettyPrint( $exec_output );
+//echo $exec_array;
 //var_dump($exec_output);
-my_print($exec_output);
-//array_walk($exec_output, 'print_row');
-//echo exec('alpr '.$image_name);
+//my_print($exec_array);
+
 ?>
--- software/web/db_functions.php
+++ software/web/db_functions.php
@@ -149,4 +149,21 @@ function prettyPrint( $json )
     return $result;
 }
 
+//http://ryanuber.com/07-31-2012/split-and-decode-json-php.html
+function json_split_objects($json)
+{
+    $q = FALSE;
+    $len = strlen($json);
+    for($l=$c=$i=0;$i<$len;$i++)
+    {   
+        $json[$i] == '"' && ($i>0?$json[$i-1]:'') != '\\' && $q = !$q;
+        if(!$q && in_array($json[$i], array(" ", "\r", "\n", "\t"))){continue;}
+        in_array($json[$i], array('{', '[')) && !$q && $l++;
+        in_array($json[$i], array('}', ']')) && !$q && $l--;
+        (isset($objects[$c]) && $objects[$c] .= $json[$i]) || $objects[$c] = $json[$i];
+        $c += ($l == 0);
+    }   
+    return $objects;
+}
+
 ?>
