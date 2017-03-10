d3b569f3ceca3312aabba2e78ac97fd38de51f7f
Jeremy Chan <jeremych1000@gmail.com>
2017-03-10 23:37:02 +0000

    prune inactive peers after 20min

--- software/bootstrapping/bootstrap/management/commands/prune_alive.py
+++ software/bootstrapping/bootstrap/management/commands/prune_alive.py
@@ -5,6 +5,17 @@ import requests, json, datetime
 
 from bootstrap import models
 
+
+def convert_timedelta(duration):
+    days, seconds = duration.days, duration.seconds
+    hours = days * 24 + seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = (seconds % 60)
+    total_hours = hours + minutes / 60 + (seconds / 60) / 60
+    total_minutes = 60 * hours + minutes + seconds / 60
+    return hours, minutes, seconds, total_hours, total_minutes
+
+
 class Command(BaseCommand):
     def add_arguments(self, parser):
         pass
@@ -14,4 +25,14 @@ class Command(BaseCommand):
 
         for i in peer_objects:
             t_delta = i.last_seen - i.first_seen
-            print(i.ip_address, i.port, t_delta)
+            hours, minutes, seconds, total_hours, total_minutes = convert_timedelta(t_delta)
+            print(hours, " ", minutes, " ", seconds, " ", total_hours, " ", total_minutes)
+
+            if total_minutes >= 20:
+                i.active = False
+                i.save()
+                print("made inactive")
+            if total_hours >= 24:
+                i.delete()
+                print("deleted")
+ 
