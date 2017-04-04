ea38312cae77b9dbf23b0d68f415753d0edc9f83
Jeremy Chan <jeremych@outlook.com>
2017-04-04 16:31:54 +0000

    bootstrap scripts

---
+++ software/peer/b1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 manage.py runcrons 'client.cron.register.Register' --force
--- software/peer/b2.sh
+++ software/peer/b2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-python3 manage.py runcrons 'client.cron.register.Register' --force
 python3 manage.py runcrons 'client.cron.get_peer_list.Get_Peer_List' --force
 python3 manage.py runcrons 'client.cron.keep_alive.Keep_Alive' --force
-python3 manage.py runcrons 'client.cron.keep_alive.peer.Keep_Alive.Peer' --force
+python3 manage.py runcrons 'client.cron.keep_alive.Keep_Alive_Peer' --force
