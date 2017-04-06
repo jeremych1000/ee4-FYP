5d8a83fceca6c0a72fa1526c9bf0756e42f8f163
Jeremy Chan <jeremych@outlook.com>
2017-04-06 17:00:56 +0800

    Update provision.sh

--- hardware/rpi/provision.sh
+++ hardware/rpi/provision.sh
@@ -5,7 +5,8 @@ sudo apt-get remove apt-listchanges
 
 sudo apt-get -y upgrade
 
-sudo apt-get -y install sudo cmake curl dos2unix emacs g++ gedit git git-core java-common java-package make perl python3 python3-pip ssh 
+sudo apt-get -y install sudo cmake curl dos2unix emacs g++ gedit git git-core htop make
+sudo apt-get -y install java-common java-package perl python3 python3-pip ssh 
 sudo apt-get -y install libtiff5-dev libopencv-dev libjpeg-dev libssl-dev
 sudo apt-get -y install apache2
 
